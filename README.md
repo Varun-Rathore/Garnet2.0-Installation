2f14b89cd7895310dc90fb6c0019fe440999865a
Varun-Rathore <53299365+Varun-Rathore@users.noreply.github.com>
2020-08-05 21:47:21 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+This is a simple shell script to Download & install Garnet along with all the software requirments<br/>
+
+## How to install
+
+<ul>
+<li>Download or this repo</li>
+<li>Run the script 'runme' with command<ul><li>$ source RunMe.sh</li></ul></li>
+<li>Sit back and relax</li>
+</ul>
---
+++ RunMe.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y g++ python3 python3-dev python-six scons swig m4 protobuf-compiler libgoogle-perftools-dev
+sudo rm -rf /home/$USER/gem5/
+git -C /home/$USER/ clone https://gem5.googlesource.com/public/gem5
+sudo cp -f ./FileSystemConfig.py /home/$USER/gem5/configs/common/FileSystemConfig.py
+sudo cp -f ./convert.py /home/$USER/gem5/build/NULL/python/m5/util/convert.py
+cd ~
+cd gem5/
+scons build/NULL/gem5.debug PROTOCOL=Garnet_standalone
