021ddcda37d2444099de8ad09bb6076000b2f158
Varun-Rathore <53299365+Varun-Rathore@users.noreply.github.com>
2020-08-05 22:10:12 +0530

    Add files via upload

--- RunMe.sh
+++ RunMe.sh
@@ -6,6 +6,13 @@ sudo rm -rf /home/$USER/gem5/
 git -C /home/$USER/ clone https://gem5.googlesource.com/public/gem5
 sudo cp -f ./FileSystemConfig.py /home/$USER/gem5/configs/common/FileSystemConfig.py
 sudo cp -f ./convert.py /home/$USER/gem5/build/NULL/python/m5/util/convert.py
+x_dir=$(pwd)
+cd ~
+cd gem5/
+scons build/NULL/gem5.debug PROTOCOL=Garnet_standalone
+cd $x_dir
+sudo cp -f ./FileSystemConfig.py /home/$USER/gem5/configs/common/FileSystemConfig.py
+sudo cp -f ./convert.py /home/$USER/gem5/build/NULL/python/m5/util/convert.py
 cd ~
 cd gem5/
 scons build/NULL/gem5.debug PROTOCOL=Garnet_standalone
