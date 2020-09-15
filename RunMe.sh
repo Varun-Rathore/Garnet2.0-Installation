b88d0bb62bc60c71ae944ff20f9a0537433fb3f3
Varun-Rathore <53299365+Varun-Rathore@users.noreply.github.com>
2020-09-15 09:44:47 +0530

    Update RunMe.sh

--- RunMe.sh
+++ RunMe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install -y g++ python3 python3-dev python-six scons swig m4 protobuf-compiler libgoogle-perftools-dev
+sudo apt install -y g++ python3 python3-dev python-six scons swig m4 protobuf-compiler libgoogle-perftools-dev libpng-dev
 sudo rm -rf /home/$USER/gem5/
 git -C /home/$USER/ clone https://gem5.googlesource.com/public/gem5
 sudo cp -f ./FileSystemConfig.py /home/$USER/gem5/configs/common/FileSystemConfig.py
